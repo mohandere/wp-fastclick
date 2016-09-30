ec48cd36c0d8fd59ee98cdcce29c85a384b1189c
mohandere <deremohan01@gmail.com>
2016-09-30 18:12:57 +0530

    first commit

---
+++ assets/js/wp-fastclick.js
@@ -0,0 +1,9 @@
+;(function () {
+	'use strict';
+
+	//To instantiate FastClick on the body, which is the recommended method of use
+	window.addEventListener('load', function() {
+	    new FastClick(document.body);
+	}, false);
+
+}());
